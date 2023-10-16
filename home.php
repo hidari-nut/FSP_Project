a1df89e9c1138971f306b3f4fe163da978d5d5e7
yuveex <alvinsetiawan2003@gmail.com>
2023-10-16 22:22:33 +0700

    Fixed Login

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <?php
-    require_once("connect.php");
+require_once("connect.php");
 ?>
 <link rel="stylesheet" href="style.css">
 <div class="container">
@@ -13,6 +13,72 @@
     </form>
 
     <table>
-        <tr><th>title</th><th>Created at</th><th>Action</th></tr>
+        <tr>
+            <th>title</th>
+            <th>Created at</th>
+            <th>Action</th>
+        </tr>
     </table>
 </div>
+
+<?php
+
+// Get user details
+
+function random_str(
+    int $length = 64,
+    string $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+): string {
+    if ($length < 1) {
+        throw new \RangeException("Length must be a positive integer");
+    }
+    $pieces = [];
+    $max = mb_strlen($keyspace, '8bit') - 1;
+    for ($i = 0; $i < $length; ++$i) {
+        $pieces []= $keyspace[random_int(0, $max)];
+    }
+    return implode('', $pieces);
+}
+
+// $iterations = 1000;
+
+// $sql = "INSERT INTO users (idusers, nama, password, salt) VALUES(?,?,?,?)";
+// $statement = $con->prepare($sql);
+
+// $iduser = "160421075";
+// $name = "Alvin Setiawan";
+// $password = "160421075";
+// $salt = random_bytes(2);
+// $salt = random_str(10);
+
+// $saltedpass = $password . $salt;
+// $finalpass = hash_pbkdf2("sha256", $password, $salt, $iterations, 64);
+
+// $statement->bind_param('ssss', $iduser, $name, $finalpass, $salt);
+// $statement->execute();
+
+// $iduser = "160421044";
+// $name = "Vincent Joewono";
+// $password = "160421044";
+// $salt = random_bytes(2);
+// $salt = random_str(10);
+
+// $saltedpass = $password . $salt;
+// $finalpass = hash_pbkdf2("sha256", $password, $salt, $iterations, 64);
+
+// $statement->bind_param('ssss', $iduser, $name, $finalpass, $salt);
+// $statement->execute();
+
+// $iduser = "160421069";
+// $name = "Made Viswanata";
+// $password = "160421069";
+// $salt = random_bytes(2);
+// $salt = random_str(10);
+
+// $saltedpass = $password . $salt;
+// $finalpass = hash_pbkdf2("sha256", $password, $salt, $iterations, 64);
+
+// $statement->bind_param('ssss', $iduser, $name, $finalpass, $salt);
+// $statement->execute();
+
+?>
