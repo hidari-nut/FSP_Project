8a7a785a7963ac14ab52f16e9ed2c6c1a2aad73c
yuveex <alvinsetiawan2003@gmail.com>
2023-10-16 22:16:46 +0700

    Updated Encryption
    Fixed Salting on PBKDF2 Encryption

--- connect.php
+++ connect.php
@@ -1,5 +1,6 @@
 <?php
-    $con = new mysqli("sql207.infinityfree.com", "epiz_34105185", "WKclieaJtD", "epiz_34105185_fspdb");
+    // $con = new mysqli("sql207.infinityfree.com", "epiz_34105185", "WKclieaJtD", "epiz_34105185_fspdb");
+    $con = new mysqli("localhost", "root", "", "fspdb");
     if ($con->connect_errno) {
         die("Failed to connect to MySQL: " . $con->connect_errno);
     } else {
