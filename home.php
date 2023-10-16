0c42071d91a76b7f14b84ecf5a314c46ad07dddc
yuveex <alvinsetiawan2003@gmail.com>
2023-10-16 21:49:32 +0700

    Added Encryption using PBKDF2

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <?php
-    require_once("connect.php");
+require_once("connect.php");
 ?>
 <link rel="stylesheet" href="style.css">
 <div class="container">
@@ -13,6 +13,57 @@
     </form>
 
     <table>
-        <tr><th>title</th><th>Created at</th><th>Action</th></tr>
+        <tr>
+            <th>title</th>
+            <th>Created at</th>
+            <th>Action</th>
+        </tr>
     </table>
 </div>
+
+<?php
+
+// Get user details
+
+// $iterations = 1000;
+
+// $sql = "INSERT INTO users (idusers, nama, password, salt) VALUES(?,?,?,?)";
+// $statement = $con->prepare($sql);
+
+// $iduser = "160421075";
+// $name = "Alvin Setiawan";
+// $password = "160421075";
+// $salt = random_bytes(2);
+// // $salt = str_shuffle("fullstackprogramming");
+
+// $saltedpass = $password . $salt;
+// $finalpass = hash_pbkdf2("sha256", $password, $salt, $iterations, 32);
+
+// $statement->bind_param('ssss', $iduser, $name, $finalpass, $salt);
+// $statement->execute();
+
+// $iduser = "160421044";
+// $name = "Vincent Joewono";
+// $password = "160421044";
+// $salt = random_bytes(2);
+// // $salt = str_shuffle("fullstackprogramming");
+
+// $saltedpass = $password . $salt;
+// $finalpass = hash_pbkdf2("sha256", $password, $salt, $iterations, 32);
+
+// $statement->bind_param('ssss', $iduser, $name, $finalpass, $salt);
+// $statement->execute();
+
+// $iduser = "160421069";
+// $name = "Made Viswanata";
+// $password = "160421069";
+// $salt = random_bytes(2);
+// // $salt = str_shuffle("fullstackprogramming");
+
+// $saltedpass = $password . $salt;
+// $finalpass = hash_pbkdf2("sha256", $password, $salt, $iterations, 32);
+
+// $statement->bind_param('ssss', $iduser, $name, $finalpass, $salt);
+// $statement->execute();
+
+?>
