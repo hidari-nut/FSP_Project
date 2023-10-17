26c65983d585c2260a71238dbca3dc69646edd41
yuveex <alvinsetiawan2003@gmail.com>
2023-10-17 17:34:45 +0700

    Update fspdb.sql

--- fspdb.sql
+++ fspdb.sql
@@ -1,108 +1,150 @@
--- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Win64 (AMD64)
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: fspdb
--- ------------------------------------------------------
--- Server version	10.4.24-MariaDB
+-- Host: 127.0.0.1
+-- Generation Time: Oct 17, 2023 at 12:33 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fspdb`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `cerita`
 --
 
-DROP TABLE IF EXISTS `cerita`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cerita` (
-  `idcerita` int(11) NOT NULL AUTO_INCREMENT,
+  `idcerita` int(11) NOT NULL,
   `judul` varchar(100) DEFAULT NULL,
-  `idusers` varchar(40) NOT NULL,
-  PRIMARY KEY (`idcerita`),
-  KEY `fk_cerita_users_idx` (`idusers`),
-  CONSTRAINT `fk_cerita_users` FOREIGN KEY (`idusers`) REFERENCES `users` (`idusers`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `idusers` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `cerita`
 --
 
-LOCK TABLES `cerita` WRITE;
-/*!40000 ALTER TABLE `cerita` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cerita` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `cerita` (`idcerita`, `judul`, `idusers`) VALUES
+(1, 'Story 1', '160421075');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `paragraf`
 --
 
-DROP TABLE IF EXISTS `paragraf`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `paragraf` (
-  `idparagraf` int(11) NOT NULL AUTO_INCREMENT,
+  `idparagraf` int(11) NOT NULL,
   `isi_paragraf` varchar(100) DEFAULT NULL,
   `tanggal_buat` timestamp NULL DEFAULT current_timestamp(),
   `idcerita` int(11) NOT NULL,
-  `idusers` varchar(40) NOT NULL,
-  PRIMARY KEY (`idparagraf`),
-  KEY `fk_paragraf_cerita1_idx` (`idcerita`),
-  KEY `fk_paragraf_users1_idx` (`idusers`),
-  CONSTRAINT `fk_paragraf_cerita1` FOREIGN KEY (`idcerita`) REFERENCES `cerita` (`idcerita`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_paragraf_users1` FOREIGN KEY (`idusers`) REFERENCES `users` (`idusers`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `idusers` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `paragraf`
 --
 
-LOCK TABLES `paragraf` WRITE;
-/*!40000 ALTER TABLE `paragraf` DISABLE KEYS */;
-/*!40000 ALTER TABLE `paragraf` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `paragraf` (`idparagraf`, `isi_paragraf`, `tanggal_buat`, `idcerita`, `idusers`) VALUES
+(1, 'This is the first paragraph of Story 1', '2023-10-17 09:37:06', 1, '160421075'),
+(2, 'This is the second and best paragraph of Story 1, Challenge me.', '2023-10-17 09:48:30', 1, '160421075'),
+(3, 'And this is the third paragraph of Story 1. Boo.', '2023-10-17 09:49:54', 1, '160421075'),
+(4, 'And at last is the fourth paragraph of Story 1. Bite me.', '2023-10-17 09:51:47', 1, '160421075');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `idusers` varchar(40) NOT NULL,
   `nama` varchar(45) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
-  `salt` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`idusers`)
+  `salt` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `users` (`idusers`, `nama`, `password`, `salt`) VALUES
+('160421044', 'Vincent Joewono', 'f18e26010e3cf82a76e644df3741e9415d1f5e8bee123350188b293fe0820226', 'ZfRh5ypvgn'),
+('160421069', 'Made Viswanata', 'bd09cb4a7c7180d90610a59e3e60e7b11b2f42b0c96b92a2c85ba94d5bc6a266', 'wR9t5bX07w'),
+('160421075', 'Alvin Setiawan', '4d9b617ed33564e986f881ef289dc14d82b345719ad1708e70f72dfcdf769a8f', '8u45ulGITA');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cerita`
+--
+ALTER TABLE `cerita`
+  ADD PRIMARY KEY (`idcerita`),
+  ADD KEY `fk_cerita_users_idx` (`idusers`);
+
+--
+-- Indexes for table `paragraf`
+--
+ALTER TABLE `paragraf`
+  ADD PRIMARY KEY (`idparagraf`),
+  ADD KEY `fk_paragraf_cerita1_idx` (`idcerita`),
+  ADD KEY `fk_paragraf_users1_idx` (`idusers`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`idusers`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cerita`
+--
+ALTER TABLE `cerita`
+  MODIFY `idcerita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `paragraf`
+--
+ALTER TABLE `paragraf`
+  MODIFY `idparagraf` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cerita`
+--
+ALTER TABLE `cerita`
+  ADD CONSTRAINT `fk_cerita_users` FOREIGN KEY (`idusers`) REFERENCES `users` (`idusers`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `paragraf`
+--
+ALTER TABLE `paragraf`
+  ADD CONSTRAINT `fk_paragraf_cerita1` FOREIGN KEY (`idcerita`) REFERENCES `cerita` (`idcerita`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_paragraf_users1` FOREIGN KEY (`idusers`) REFERENCES `users` (`idusers`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-10-08 18:12:52
